1925910ada96be99bfc40b1a4d5a8a4fadab44b7
Gabriel <gabrielwilliam234@gmail.com>
2025-04-02 19:23:38 -0300

    Add files via upload

---
+++ calculadora-completa-insana/App.js
@@ -0,0 +1,97 @@
+import React, {useState} from 'react';
+import {View, Button, TextInput} from 'react-native';
+import {styles} from './estilo/styles';
+
+const App = () => {
+  const [num1, setnum1] = useState('');
+  const [num2, setnum2] = useState('');
+  const [resul, setresul] = useState('');
+
+  operSoma = () => {
+    const numero1 = parseFloat(num1);
+    const numero2 = parseFloat(num2);
+
+    if (!isNaN(numero1) && !isNaN(numero2)){
+      setresul (numero1 + numero2);
+    } else {
+      setresul ("Número Inválido!");
+    }
+  }
+  
+  operMulti = () => {
+    const numero1 = parseFloat(num1);
+    const numero2 = parseFloat(num2);
+
+    if (!isNaN(numero1) && !isNaN(numero2)){
+      setresul (numero1 * numero2);
+    } else {
+      setresul("Número Inválido!");
+    }
+  }
+  
+  operSubtrai = () => {
+    const numero1 = parseFloat(num1);
+    const numero2 = parseFloat(num2);
+
+    if (!isNaN(numero1) && !isNaN(numero2)){
+      setresul (numero1 - numero2);
+    } else {
+      setresul ("Número Inválido!");
+    }
+  }
+  
+  operDivide = () => {
+    const numero1 = parseFloat(num1);
+    const numero2 = parseFloat(num2);
+
+    if (!isNaN(numero1) && !isNaN(numero2)){
+      setresul (numero1/numero2);
+    } else {
+      setresul ("Número Inválido!")
+    }
+  }
+
+ return(
+    <View style={styles.container}>
+      <TextInput 
+        style = {styles.input}
+        placeholder="Digite um número."
+        keyboardType="numeric"
+        value={num1}
+        onChangeText = {setnum1}
+      />
+      <TextInput 
+        style = {styles.input}
+        placeholder="Digite a sua nota."
+        keyboardType="numeric"
+        value={num2}
+        onChangeText = {setnum2}
+      />
+     
+      <View style = {styles.butao}>
+        <Button title="+" onPress={operSoma}/>
+      </View>
+      <View style = {styles.butao}>
+        <Button title="-" onPress={operSubtrai}/>
+      </View>
+      <View style = {styles.butao}>
+        <Button title="x" onPress={operMulti}/>
+      </View>
+      <View style = {styles.butao}>
+      <Button title="/" onPress={operDivide}/>
+       </View>
+
+      <TextInput 
+        style = {styles.input}
+        placeholder="Resultado"
+        value={resul}
+        editable={false}
+      />
+
+      </View>
+  );
+};
+
+
+export default App;
+  
